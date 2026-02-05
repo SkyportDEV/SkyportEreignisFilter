9948cfb46e7f8eac147f7e896ac15255812f0169
SkyportDEV <a.schieder@skyport-group.de>
2026-02-05 08:47:41 +0100

    Update SkyportEreignisFilterServiceProvider.php

--- src/Providers/SkyportEreignisFilterServiceProvider.php
+++ src/Providers/SkyportEreignisFilterServiceProvider.php
@@ -6,8 +6,8 @@ use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\EventProcedures\Services\EventProceduresService;
 use Plenty\Modules\EventProcedures\Services\Entries\ProcedureEntry;
 use SkyportEreignisFilter\EventProcedures\OrderFilters;
-use SkyportEreignisFilter\Providers\RouteServiceProvider;
-use SkyportEreignisFilter\Providers\BackendServiceProvider;
+use SkyportEreignisFilter\Providers\SkyportEreignisFilterRouteServiceProvider;
+use SkyportEreignisFilter\Providers\SkyportEreignisFilterBackendServiceProvider;
 
 class SkyportEreignisFilterServiceProvider extends ServiceProvider
 {
@@ -28,7 +28,7 @@ class SkyportEreignisFilterServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->getApplication()->register(RouteServiceProvider::class);
-        $this->getApplication()->register(BackendServiceProvider::class);
+        $this->getApplication()->register(SkyportEreignisFilterRouteServiceProvider::class);
+        $this->getApplication()->register(SkyportEreignisFilterBackendServiceProvider::class);
     }
 }
